cc0b6099fd39ce0a975a325efe950e89f9d77802
Andrew Krause <andrewkrause@Andrews-MacBook-Pro.local>
2020-11-14 20:49:41 -0600

    remove file reference because of dox

--- src/components/sections/AboveTheFold.js
+++ src/components/sections/AboveTheFold.js
@@ -8,12 +8,6 @@ const AboveTheFold = ({ title, description }) => {
         <h1>{title}</h1>
         <p>{description}</p>
         <div className="button-container">
-          <Button
-            url={process.env.PUBLIC_URL + "files/AndrewKrauseResume2019.pdf"}
-            icon="newspaper"
-            text="Resume"
-            external={true}
-          />
           <Button url="#contact" icon="brain" text="Projects" />
         </div>
       </div>
